aa337727bc6135613c20f62d08a47e819fdc2440
Mohamed Alaa <mathematecs1@gmail.com>
2026-05-14 14:30:29 +0300

    Added activation function formulas to ffn.h

--- ONN/include/ffn.h
+++ ONN/include/ffn.h
@@ -5,5 +5,11 @@
         net = w1*x1 + w2*x2 + b
         y = f(net)
     where f : 
-        sigmoid 
+        sigmoid : f(x) = 1/(1 + exp(-x))
+        tanh    : f(x) = (exp(x) - exp(-x)) / (exp(x) + exp(-x))
+        relu    : f(x) = max(0, x)
+        softmax : f(x) = exp(x) / sum(exp(x))
+    
+        
+    
 */
