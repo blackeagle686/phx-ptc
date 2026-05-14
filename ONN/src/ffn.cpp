9e0e4dcc517a673ae9b80a03497db2adf2103d92
Mohamed Alaa <mathematecs1@gmail.com>
2026-05-14 15:17:33 +0300

    Added FFN class constructor and basic methods

--- ONN/src/ffn.cpp
+++ ONN/src/ffn.cpp
@@ -2,5 +2,44 @@
 
 FFN::FFN()
 {
+    input = WaveChannel();
+    output = WaveChannel();
+    weights = std::vector<Complex>();
+    bias = std::vector<Complex>();
+    activation = "";
+}
+
+FFN::~FFN()
+{
+
+}
+
+void FFN::set_input(WaveChannel& input)
+{
+    this->input = input;
+}
+
+void FFN::set_weights(std::vector<Complex>& weights)
+{
+    this->weights = weights;
+}
+
+void FFN::set_bias(std::vector<Complex>& bias)
+{
+    this->bias = bias;
+}
+
+void FFN::set_activation(std::string activation)
+{
+    this->activation = activation;
+}
+
+void FFN::forward()
+{
+    
+}
+
+void FFN::backward()
+{
     
 }
